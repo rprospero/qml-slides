0178f5acb6148b3ba1ebce1ca1f233527d029cae
Adam Washington <adam.washington@stfc.ac.uk>
2023-06-28 11:00:21 +0100

    More duplicate files to interactive

---
+++ interactive/model.py
@@ -0,0 +1,41 @@
+import numpy as np
+from PySide2.QtCore import QObject, Property, Signal, QUrl
+from PySide2.QtQml import QQmlApplicationEngine, qmlRegisterType
+
+class Stats(QObject):
+        _data = None # The numbers to calculate over
+        _filename = "";
+
+        def __init__(self):
+            """ Create the Stats object """
+            QObject.__init__(self)
+
+        dataChanged = Signal()
+
+        @Property(QUrl, notify=dataChanged)
+        def filename(self):
+                """ The name of the data file """
+                return self._filename
+
+        @filename.setter
+        def filename(self, name):
+                """ Load a new data file """
+                self._filename = name
+                self._data = np.loadtxt(name.toLocalFile())
+                self.dataChanged.emit()
+
+        @Property(float, notify=dataChanged)
+        def x_mean(self):
+            """ The mean of the first column """
+            if self._data is None:
+                    return 0
+            return np.mean(self._data[:, 0])
+
+        @Property(float, notify=dataChanged)
+        def y_mean(self):
+            """ The mean of the second column """
+            if self._data is None:
+                    return 0
+            return np.mean(self._data[:, 1])
+
+qmlRegisterType(Stats, "Tutorial", 1, 0, "Stats")
