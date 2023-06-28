f06f5b1f68574db0dc01dc3cd5c3ed2afc1f2e75
Adam Washington <adam.washington@stfc.ac.uk>
2023-06-28 10:19:16 +0100

    Copy duplicate files to display folder

---
+++ display/model.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PySide2.QtCore import QObject, Property
+from PySide2.QtQml import QQmlApplicationEngine, qmlRegisterType
+class Stats(QObject):
+  _data = None
+  def __init__(self):
+        QObject.__init__(self)
+        self.data_ = np.loadtxt("data.txt")
+  @Property(float)
+  def x_mean(self):
+        return np.mean(self.data_[:, 0])
+  @Property(float)
+  def y_mean(self):
+        return np.mean(self.data_[:, 1])
+qmlRegisterType(Stats, "Tutorial", 1, 0, "Stats")
